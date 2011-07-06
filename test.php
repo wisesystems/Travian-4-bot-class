b5cfcf6766812e9438b975584cc473c1951324fa
Christian <x00bit@gmail.com>
2011-07-06 12:55:27 +0300

    Usage examples update

--- test.php
+++ test.php
@@ -4,17 +4,17 @@ include('travianBot.php');
 
 $travianBot = new travianBot();
 $travianBot->setCookiesPath('/tmp/cookiest.txt');
-$travianBot->setMainUrl('http://.travian./');
-$travianBot->setLoginData( 'x00bit', '' );
+$travianBot->setMainUrl('http://tx3.travian.com.au/');
+$travianBot->setLoginData( 'x00bit', 'sxsvsdha17b' );
 
 if(!$travianBot->isLoged())
         $travianBot->loginToTravian();
 
 /*
 $upgrade = new travianUpgradeRes;
-$upgrade->setResourceId(7);
+$upgrade->setResourceId(12);
 $upgrade->exec();
-var_dump($upgrade->execSuccess());
+echo $upgrade->getBuildTime()."\n";
 */
 /*
 $attack = new travianAttack;
@@ -23,9 +23,14 @@ $attack->setCoordonates( '-85' , '70' );
 $attack->setTroops(0,0,0,0,0,23);
 $attack->exec();
 */
-/*
-$send = new travianSendResources;
+
+$send = new travianSendResources; echo $send->getTotalCapacity();
 $send->setResNumber(1, 1, 1, 1);
 $send->setVillageCoords(-85, 70);
-$send->exec();
+//$send->exec();
+
+/*
+$troops = new travianTroopsNum();
+print_r( $troops->get() );
 */
+ 
